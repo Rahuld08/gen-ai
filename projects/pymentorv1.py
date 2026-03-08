2573946a622d757c5489ee45349e4faed275f8ad
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2026-03-08 17:11:19 +0530

    pymentorv1

--- projects/pymentorv1.py
+++ projects/pymentorv1.py
@@ -1,18 +1,21 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 
+
 def get_open_ai_client():
     load_dotenv()
     return OpenAI()
 
+
 def chat_with_ai(question):
-    response=client.responses.create(model="gpt-5.1",input=question)
+    response = client.responses.create(model="gpt-5.1", input=question)
     print("AI  :=>", response.output_text)
 
+
 try:
-    client=get_open_ai_client()
+    client = get_open_ai_client()
     while True:
-        question=input("Your:=> ")
+        question = input("Your:=> ")
         if question.lower() == "exit":
             break
         chat_with_ai(question)
